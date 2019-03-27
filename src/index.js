fc389ece07d7dfae8f590d7dcba922734ac30a73
pac5man <elad@puresec.io>
2019-03-27 15:16:32 +0200

    change to object assign

--- src/index.js
+++ src/index.js
@@ -290,9 +290,9 @@ class RavenLambdaWrapper {
 			captureErrors:              parseBoolean(_.get(process.env, "SENTRY_CAPTURE_ERRORS"),    true),
 			captureUnhandledRejections: parseBoolean(_.get(process.env, "SENTRY_CAPTURE_UNHANDLED"), true),
 			captureMemoryWarnings:      parseBoolean(_.get(process.env, "SENTRY_CAPTURE_MEMORY"),    true),
-			captureTimeoutWarnings:     parseBoolean(_.get(process.env, "SENTRY_CAPTURE_TIMEOUTS"),  true),
-			printEventToStdout: 				parseBoolean(_.get(process.env, "SENTRY_PRINT_EVENT_TO_STDOUT"), false),
-			filterEventsFields: 				_.get(process.env, "SENTRY_FILTER_EVENT_FIELDS", ""),
+			captureTimeoutWarnings:			parseBoolean(_.get(process.env, "SENTRY_CAPTURE_TIMEOUTS"),  true),
+			printEventToStdout:					parseBoolean(_.get(process.env, "SENTRY_PRINT_EVENT_TO_STDOUT"), false),
+			filterEventsFields:					_.get(process.env, "SENTRY_FILTER_EVENT_FIELDS", ""),
 			ravenClient: null
 		};
 
